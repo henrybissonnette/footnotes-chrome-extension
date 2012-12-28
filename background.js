8f47feda361828c7ceffd40d5b4c66b7473ec788
Henry Bissonnette <henryb@groupon.com>
2012-12-27 19:48:29 -0600

    first commit, bad state

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
-
+console.log('background running');
 chrome.browserAction.onClicked.addListener(function(){
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
+      $.ajax({
+        url: 'https://birdcage-uat.groupondev.com/v1/environments.json',
+        type: 'get',
+        dataType: 'json',
+        success: function(environments){
+          console.log('response: ' + environments)
+        }
+      });
       console.log(response.farewell);
     });
   });
 });
+
 
--- footnotes.js
+++ footnotes.js
@@ -1,93 +1,29 @@
-// $(document).ready(function(){
-//   var footnotes = $('<div>').addClass('footnotes minimized');
-//   $('body').prepend(footnotes);
-//   footnotes.text('<');
 
-//   footnotes.click(function(){
-//     if(footnotes.hasClass('minimized')){
-//       $(this).animate({width:'150px'});
-//       $(this).removeClass('minimized');
-//       $(this).text('footnotes');      
-//     }
-//     else{
-//       $(this).animate({width:'30px'});
-//       $(this).addClass('minimized');
-//       $(this).text('<');         
-//     }
-//   });
-// });
 
-window.withfootnotes = {
+(function(){
+  window.withfootnotes = {}
+  $('head').append('<script type="text/javascript" src="http://withfootnotes.com/api?url=' 
+      + window.location.href + 
+      '&callback=true"></script>')
+  var slideBase = '\
+  <a id="slider" href="#footnotes_modal" style="display:none">null</a>\
+  <div id="footnotes_modal" style="display:none">\
+  <h1>Footnotes</h1>\
+  </div>\
+  ';
 
-}
-
-withfootnotes.main = function(){
-  var footnotes = $('<div>').addClass('footnotes minimized');
-  footnotes.text('<');
-  $('body').prepend(footnotes);
-  $('body').prepend('<div id="modal" style="display:none"><h2>Modal</h2><a href="javascript:$.pageslide.close()">Close</a></div>'
-);
-  footnotes.css({'margin-top':'-170px'});
-  footnotes.html('<a class="slider" href="#modal">pageslide</a>'); 
-  $('a.slider').pageslide({direction:'left'});
-
-   return footnotes;
-}();
-
-withfootnotes.show = function(){
-  $(window.withfootnotes.main).addClass('deployed');
-  $(window.withfootnotes.main).animate({'margin-top':'0px'});
-};
-
-withfootnotes.main.toggleMinimized = function(){
-  var f = window.withfootnotes.main
-  if(f.hasClass('minimized')){
-    $(f).animate({width:'150px'});
-    $(f).removeClass('minimized');
-    $(f).html('<a class="slider" href="#modal">pageslide</a>'); 
-    $('a.slider').pageslide({direction:'left'});
-    withfootnotes.main.slidebutton();     
-  }
-  else{
-    $(f).animate({width:'30px'});
-    $(f).addClass('minimized');
-    $(f).text('<');         
-  }  
-};
-
-withfootnotes.main.click(function(){
-  withfootnotes.main.toggleMinimized();
-});
-
-// withfootnotes.main.slidebutton = function(){
-//   this.find('a.slider').click(function(){
-//     alert('pageslide clicked');
-//     $('a.slider').pageslide();
-//   });  
-// }
-
-withfootnotes.hide = function(){
-  if (!window.withfootnotes.main.hasClass('minimized')){
-    window.withfootnotes.main.toggleMinimized();
-  }
-  $(window.withfootnotes.main).removeClass('deployed');
-  $(window.withfootnotes.main).animate({'margin-top':'-170px'});
-};
-
+  $('body').prepend(slideBase);
+  $("#slider").pageslide({ direction: "left" });
+})();
 
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-    $('a.slider').click()
-    // if ($(window.withfootnotes.main).hasClass('deployed')){
-    //   window.withfootnotes.hide();
-    // }
-    // else{
-    //   window.withfootnotes.show();
-    // }
+    $('a#slider').click()
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
     if (request.greeting == "hello")
       sendResponse({farewell: "goodbye"});
 });
+
 
