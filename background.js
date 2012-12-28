ac1c4d4926c3a2183fa7d8f495df47af1d64f867
Henry Bissonnette <henryb@groupon.com>
2012-12-28 11:11:33 -0600

    working state restored

--- background.js
+++ background.js
@@ -1,15 +1,7 @@
-console.log('background running');
+console.log('background page is alive');
 chrome.browserAction.onClicked.addListener(function(){
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
-      $.ajax({
-        url: 'https://birdcage-uat.groupondev.com/v1/environments.json',
-        type: 'get',
-        dataType: 'json',
-        success: function(environments){
-          console.log('response: ' + environments)
-        }
-      });
       console.log(response.farewell);
     });
   });
--- footnotes.js
+++ footnotes.js
@@ -2,16 +2,12 @@
 
 (function(){
   window.withfootnotes = {}
-  $('head').append('<script type="text/javascript" src="http://withfootnotes.com/api?url=' 
-      + window.location.href + 
-      '&callback=true"></script>')
   var slideBase = '\
   <a id="slider" href="#footnotes_modal" style="display:none">null</a>\
   <div id="footnotes_modal" style="display:none">\
   <h1>Footnotes</h1>\
   </div>\
   ';
-
   $('body').prepend(slideBase);
   $("#slider").pageslide({ direction: "left" });
 })();
