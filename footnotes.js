9a0e2d6aa507fceecdc850a2db0caea33a696675
Henry Bissonnette <henryb@groupon.com>
2013-01-02 09:44:00 -0600

    kill everything, some style changes

--- footnotes.js
+++ footnotes.js
@@ -1,12 +1,6 @@
 
 
-Footnotes.slideBase = '\
-    <a id="slider" href="" style="display:none">null</a>\
-    <div id="footnotes_modal" style="display:none">\
-    <h1>Footnotes</h1>\
-    <div id="right_bar"></div>\
-    </div>\
-    '
+Footnotes.slideBase = '<a id="slider" href="" style="display:none">null</a>'
 
 Footnotes.isOpen = false
   
@@ -30,34 +24,12 @@ Footnotes.setQuery = function(url){
 Footnotes.initListener = function(){
     chrome.extension.onMessage.addListener(
       function(request, sender, sendResponse) {
-        this.isOpen = !this.isOpen
-        if (this.isOpen) {
-          Footnotes.renderNotes(request.notes);
-        }
-        else {
-          $('.note').remove();
-        }
         $('a#slider').click();
-        console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
         if (request.greeting == "hello"){
           sendResponse({farewell: "goodbye"});
         }
     });    
   },
-
-Footnotes.renderNotes = function(notes){
-    notes = _.map(notes, function(noteString){
-      return JSON.parse(noteString)
-    })
-    var questions = new Footnotes.Collections.Questions(notes)
-    var overlay = new Footnotes.Views.OverlayView({
-      questions: questions
-    })   
-    overlay.render()
-  }
-
 
 Footnotes.initialize()
 
